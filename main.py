094bd3c85828e9a81d153510a39d99f09f55fa6f
sydykovislam <sydykovislam03@gmail.com>
2020-12-22 08:36:43 +0600

    Add url creater

--- main.py
+++ main.py
@@ -3,3 +3,19 @@ import telebot
 
 
 bot = telebot.TeleBot('botsId')
+
+def youtube(url):
+    options = {
+        'format': 'bestaudio/best',
+        'keepvideo': False,
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }]
+    }
+    
+    with youtube_dl.YoutubeDL(options) as ydl:
+        res = ydl.extract_info(url, download=False)
+        return res['url']
+
